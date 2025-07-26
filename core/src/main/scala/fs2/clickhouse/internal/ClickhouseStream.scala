9b51cc1b997293b810be0ef01cb129baa9d6f9d6
ref <yau.ref@gmail.com>
2025-07-27 00:34:09 +0300

    add comment about autoclosable

--- core/src/main/scala/fs2/clickhouse/internal/ClickhouseStream.scala
+++ core/src/main/scala/fs2/clickhouse/internal/ClickhouseStream.scala
@@ -10,12 +10,17 @@ object ClickhouseStream {
   def http[F[_]: Async](
     host: String,
     port: Int,
-    auth: Auth 
-  ): Resource[F, ClickhouseClient[F]] = 
-    Resource
-      .fromAutoCloseable(Async[F].delay(HttpClient.newHttpClient()))
-      .map( httpClient =>
-        ???
-      )
-      
+    auth: Auth
+  ): Resource[F, ClickhouseClient[F]] = {
+
+    // this can cause problems when compiling on older jdks
+    // because HttpClient has been updated to be AutoCloseable in Java 21 only
+    // so if you see 'HttpClient is not AutoClosable' error you know why
+
+    Resource.fromAutoCloseable(Async[F].delay(HttpClient.newHttpClient()))
+
+    ???
+  }
+
+
 }
