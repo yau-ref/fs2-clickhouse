f0f2087eb2fbb7cadac0c972854983d5f541c970
ref <yau.ref@gmail.com>
2025-07-27 01:37:33 +0300

    use client in stream

--- core/src/main/scala/fs2/clickhouse/internal/ClickhouseStream.scala
+++ core/src/main/scala/fs2/clickhouse/internal/ClickhouseStream.scala
@@ -1,7 +1,6 @@
 package fs2.clickhouse.internal
 
-import cats.effect.{Async, IO, Resource}
-import fs2.clickhouse.internal.{Auth, ClickhouseClient }
+import cats.effect.{Async, Resource}
 
 import java.net.http.HttpClient
 
@@ -9,18 +8,15 @@ object ClickhouseStream {
   
   def http[F[_]: Async](
     host: String,
-    port: Int,
+    port: Int = 8123,
     auth: Auth = NoAuth
   ): Resource[F, ClickhouseClient[F]] = {
-
     // this can cause problems when compiling on older jdks
     // because HttpClient has been updated to be AutoCloseable in Java 21 only
     // so if you see 'HttpClient is not AutoClosable' error you know why
-
-    Resource.fromAutoCloseable(Async[F].delay(HttpClient.newHttpClient()))
-
-    ???
+    Resource
+      .fromAutoCloseable(Async[F].delay(HttpClient.newHttpClient()))
+      .map(javaHttpClient => new ClickhouseHTTPClient[F](javaHttpClient, host, port, auth))
   }
-
 
 }
