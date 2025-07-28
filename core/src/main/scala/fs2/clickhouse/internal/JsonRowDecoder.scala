50a12c0aa45bdea2af25fc803699799584d493c4
ref <yau.ref@gmail.com>
2025-07-28 21:52:46 +0300

    update decoder signature

--- core/src/main/scala/fs2/clickhouse/internal/JsonRowDecoder.scala
+++ core/src/main/scala/fs2/clickhouse/internal/JsonRowDecoder.scala
@@ -1,16 +1,17 @@
 package fs2.clickhouse.internal
 
-// TODO: need MonadicError here or some other way to represent fail
+import cats.data.EitherT
+
 // TODO: same for encoding
 trait JsonRowDecoder[F[_], T] {
 
-  def decode(json: String): F[T]
+  type Err <: Throwable
+  type DecodedRow = EitherT[F, T, Err]
+  def decode(json: String): DecodedRow
 
 }
 
-
 object JsonRowDecoder {
-
 
   // TODO: impl and move to own sub project to keep pluggable
   implicit def circeWrapper[F[_], T](): JsonRowDecoder[F, T] = ???
