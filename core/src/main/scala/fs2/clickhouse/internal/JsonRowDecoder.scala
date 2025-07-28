b8b1933e5182c10c68ead6ad9f998d149a51fd27
ref <yau.ref@gmail.com>
2025-07-28 21:35:09 +0300

    draft decoder

--- core/src/main/scala/fs2/clickhouse/internal/JsonRowDecoder.scala
+++ core/src/main/scala/fs2/clickhouse/internal/JsonRowDecoder.scala
@@ -1,8 +1,18 @@
 package fs2.clickhouse.internal
 
 // TODO: need MonadicError here or some other way to represent fail
+// TODO: same for encoding
 trait JsonRowDecoder[F[_], T] {
 
   def decode(json: String): F[T]
-  
+
+}
+
+
+object JsonRowDecoder {
+
+
+  // TODO: impl and move to own sub project to keep pluggable
+  implicit def circeWrapper[F[_], T](): JsonRowDecoder[F, T] = ???
+
 }
