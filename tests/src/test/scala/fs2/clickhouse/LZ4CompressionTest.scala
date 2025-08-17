1386ac0f50c2b826bbff2855e056ab1cd185746a
ref <yau.ref@gmail.com>
2025-08-18 01:04:00 +0300

    add simple test for lz4

---
+++ tests/src/test/scala/fs2/clickhouse/LZ4CompressionTest.scala
@@ -0,0 +1,41 @@
+package fs2.clickhouse
+
+import cats.effect.IO
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import fs2.clickhouse.internal.LZ4Compression
+import org.scalatest.Inspectors
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import cats.effect.unsafe.implicits.global
+
+class LZ4CompressionTest
+  extends AnyWordSpec
+    with Matchers
+    with Inspectors
+    with TestContainerForAll
+    with TestContainerHelpers
+    with WithConnection {
+
+  "LZ4Compression" should {
+    "compress data" in {
+      val pipe =
+        LZ4Compression
+          .defaultInstance
+          .decompress[IO]
+
+      val testData = "BCJNGGRApwsAAIBoZWxsb3dvcmxkCgAAAADxpNsz"
+      val decoded =
+        fs2.Stream
+          .emits(List(testData))
+          .through(fs2.text.base64.decode[IO])
+          .through(pipe)
+          .compile
+          .toVector
+          .map(bytes => new String(bytes.toArray))
+          .unsafeRunSync()
+
+      decoded shouldBe "helloworld\n"
+    }
+  }
+
+}
