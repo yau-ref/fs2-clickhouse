f46329b0801f0177c608ea32f3f95daf01d1ca6b
ref <yau.ref@gmail.com>
2025-08-18 00:25:19 +0300

    compression test draft

---
+++ tests/src/test/scala/fs2/clickhouse/CompressionTest.scala
@@ -0,0 +1,26 @@
+package fs2.clickhouse
+
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import org.scalatest.Inspectors
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class CompressionTest
+  extends AnyWordSpec
+    with Matchers
+    with Inspectors
+    with TestContainerForAll
+    with TestContainerHelpers
+    with WithConnection {
+
+  // checks compressions:
+  // 1. compressions should use correct accept-types
+  // 2. pipes should really compress/decompress
+  // 3. compression should actually work with ch 
+
+  // check integration:
+  // 1. the client actually uses compression if provided
+  // 2. ch returns and accepts compressed data
+  // 3. http client correctly handles wrong compression types
+
+}
