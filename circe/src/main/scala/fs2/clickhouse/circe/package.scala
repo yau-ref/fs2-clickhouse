7d2d64411926c45e3c640af17dc5151615c85f64
ref <yau.ref@gmail.com>
2026-08-07 20:55:18 +0300

    add enc wrapper to package

--- circe/src/main/scala/fs2/clickhouse/circe/package.scala
+++ circe/src/main/scala/fs2/clickhouse/circe/package.scala
@@ -3,6 +3,7 @@ package fs2.clickhouse
 import cats.data.EitherT
 import cats.effect.kernel.Sync
 import fs2.clickhouse.internal.JsonRowDecoder
+import fs2.clickhouse.internal.JsonRowEncoder
 import io.circe
 
 package object circe {
@@ -27,6 +28,26 @@ package object circe {
     new JsonRowDecoder[F, T] {
       override def decode(json: String): DecodedRow =
         EitherT(Sync[F].delay(circe.parser.decode[T](json)))
+    }
+
+  /** Conversion from existing circe encoder
+    *
+    * Example:
+    * {{{
+    *   import io.circe.generic.auto._
+    *   import fs2.clickhouse.circe.circeEncoderWrapper
+    *
+    *   case class User(name: String, age: Int)
+    *
+    *   val stream: Pipe[F, User, Nothing] =
+    *     clickhouse.insert[User]("insert into test.users format JSONEachRow")
+    * }}}
+    */
+  implicit def circeEncoderWrapper[F[_]: Sync, T: circe.Encoder]
+    : JsonRowEncoder[F, T] =
+    new JsonRowEncoder[F, T] {
+      override def encode(row: T): EncodedRow =
+        EitherT.right(Sync[F].delay(circe.Encoder[T].apply(row).noSpaces))
     }
 
 }
