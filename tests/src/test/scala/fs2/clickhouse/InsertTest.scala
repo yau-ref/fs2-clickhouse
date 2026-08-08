aa2b96a37a196459f06496db16f9d620aff2044e
ref <yau.ref@gmail.com>
2026-08-09 00:17:12 +0300

    add test for batch insertions

--- tests/src/test/scala/fs2/clickhouse/InsertTest.scala
+++ tests/src/test/scala/fs2/clickhouse/InsertTest.scala
@@ -103,6 +103,47 @@ class InsertTest
 
       inserted should contain(user)
     }
+
+    "insert data spanning multiple batches" in withContainers { implicit clickHouseContainer =>
+      val users = TestData.users(250)
+
+      withConnection { connection =>
+        val statement = connection.createStatement()
+        statement.execute("create database if not exists test")
+        statement.execute("create table test.batched_users (name text, age Int8) Engine = MergeTree() order by name")
+      }
+
+      fs2.Stream
+        .resource(
+          ClickhouseStream
+            .http[IO](
+              hostName,
+              httpApiPort,
+              Credentials(username, Some(password)),
+              compression = NoCompression
+            )
+        )
+        .flatMap(clickhouse =>
+          fs2.Stream
+            .emits(users)
+            // a batch size that doesn't evenly divide the number of rows,
+            // so the last batch is partial and still has to be flushed
+            .through(clickhouse.insert[User]("insert into test.batched_users format JSONEachRow", maxBatchSize = 37))
+        )
+        .compile.drain.unsafeRunSync()
+
+      val inserted = withConnection { connection =>
+        val statement = connection.createStatement()
+        val resultSet = statement.executeQuery("select name, age from test.batched_users")
+        val buffer = scala.collection.mutable.ArrayBuffer.empty[User]
+        while (resultSet.next())
+          buffer += User(resultSet.getString("name"), resultSet.getInt("age"))
+        buffer.toVector
+      }
+
+      inserted should contain allElementsOf users
+      inserted should have size users.size.toLong
+    }
   }
 
 }
