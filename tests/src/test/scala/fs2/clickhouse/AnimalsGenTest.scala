5134d219b466935a223ff0f15a6c22898793e988
ref <yau.ref@gmail.com>
2026-08-11 00:37:31 +0300

    add a proprty based test for insert and query

---
+++ tests/src/test/scala/fs2/clickhouse/AnimalsGenTest.scala
@@ -0,0 +1,81 @@
+package fs2.clickhouse
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import fs2.clickhouse.TestData.Animal
+import fs2.clickhouse.circe.*
+import fs2.clickhouse.compression.NoCompression
+import fs2.clickhouse.internal.Credentials
+import io.circe.generic.auto.*
+import org.scalatest.Inspectors
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class AnimalsGenTest
+  extends AnyWordSpec
+    with Matchers
+    with Inspectors
+    with TestContainerForAll
+    with TestContainerHelpers
+    with WithConnection {
+
+  "client" should {
+    
+    "insert property-generated data and query it back" in withContainers { implicit clickHouseContainer =>
+      val animals = TestData.animals()
+
+      withConnection { connection =>
+        val statement = connection.createStatement()
+        statement.execute("create database if not exists test")
+        statement.execute(
+          """create table test.animals (
+            |  name String,
+            |  animalType String,
+            |  whenLived String,
+            |  sizeInMeters Float64,
+            |  whenDiscovered Int32,
+            |  location String,
+            |  color Nullable(String),
+            |  food String
+            |) Engine = MergeTree() order by name""".stripMargin
+        )
+      }
+
+      fs2.Stream
+        .resource(
+          ClickhouseStream
+            .http[IO](
+              hostName,
+              httpApiPort,
+              Credentials(username, Some(password)),
+              compression = NoCompression
+            )
+        )
+        .flatMap(clickhouse =>
+          fs2.Stream
+            .emits(animals)
+            .through(clickhouse.insert[Animal]("insert into test.animals format JSONEachRow"))
+        )
+        .compile.drain.unsafeRunSync()
+
+      val result =
+        fs2.Stream
+          .resource(
+            ClickhouseStream
+              .http[IO](
+                hostName,
+                httpApiPort,
+                Credentials(username, Some(password)),
+                compression = NoCompression
+              )
+          ).flatMap(clickhouse =>
+            clickhouse.query[Animal]("select * from test.animals")
+          ).compile.toVector.unsafeRunSync()
+
+      result should have size animals.size.toLong
+      result should contain allElementsOf animals
+    }
+  }
+
+}
--- tests/src/test/scala/fs2/clickhouse/TestData.scala
+++ tests/src/test/scala/fs2/clickhouse/TestData.scala
@@ -1,5 +1,7 @@
 package fs2.clickhouse
 
+import org.scalacheck.Gen
+
 object TestData {
 
   // testing data
@@ -10,5 +12,93 @@ object TestData {
       .map(i => User(s"user-$i", i % 100))
       .toVector
 
+  case class Animal(
+    name: String,
+    animalType: String,
+    whenLived: String,
+    sizeInMeters: Double,
+    whenDiscovered: Int,
+    location: String,
+    color: Option[String],
+    food: String
+  )
+
+  // property-based generator for Animal rows, see AnimalsGenTest
+  val animalGen: Gen[Animal] =
+    for {
+      name <- Gen.oneOf(
+        "Tyrannosaurus rex",
+        "Triceratops horridus",
+        "Stegosaurus stenops",
+        "Brachiosaurus altithorax",
+        "Spinosaurus aegyptiacus",
+        "Diplodocus carnegii",
+        "Velociraptor mongoliensis",
+        "Ankylosaurus magniventris",
+        "Parasaurolophus walkeri",
+        "Iguanodon bernissartensis",
+        "Allosaurus fragilis",
+        "Deinonychus antirrhopus",
+        "Pachycephalosaurus wyomingensis",
+        "Compsognathus longipes",
+        "Archaeopteryx lithographica"
+      )
+      animalType <- Gen.oneOf(
+        "Dinosaur - Theropod",
+        "Dinosaur - Sauropod",
+        "Dinosaur - Ceratopsian",
+        "Dinosaur - Ornithopod",
+        "Dinosaur - Stegosaur",
+        "Dinosaur - Ankylosaur",
+        "Dinosaur - Hadrosaur",
+        "Dinosaur - Sauropodomorph",
+        "Synapsid - Cynodont",
+        "Synapsid - Dicynodont",
+        "Synapsid - Mammal",
+        "Archosauriform",
+        "Archosauromorph"
+      )
+      whenLived <- Gen.oneOf(
+        "Late Triassic",
+        "Middle Triassic",
+        "Early Jurassic",
+        "Late Jurassic",
+        "Early Cretaceous",
+        "Late Cretaceous"
+      )
+      sizeInMeters <- Gen.choose(0.2, 30.0)
+      whenDiscovered <- Gen.choose(1820, 2025)
+      location <- Gen.oneOf(
+        "Montana, USA",
+        "Colorado, USA",
+        "Wyoming, USA",
+        "Alberta, Canada",
+        "Liaoning, China",
+        "Bavaria, Germany",
+        "Patagonia, Argentina",
+        "Bahariya Oasis, Egypt",
+        "Isle of Wight, UK"
+      )
+      color <- Gen.oneOf(
+        None,
+        Some("brown"),
+        Some("green"),
+        Some("gray"),
+        Some("reddish-brown"),
+        Some("dark green")
+      )
+      food <- Gen.oneOf(
+        "Carnivore",
+        "Herbivore",
+        "Omnivore",
+        "Piscivore",
+        "Insectivore",
+        "Herbivore/Omnivore",
+        "Carnivore/Omnivore"
+      )
+    } yield Animal(name, animalType, whenLived, sizeInMeters, whenDiscovered, location, color, food)
+
+  def animals(num: Int = 300): Vector[Animal] =
+    LazyList.continually(Gen.listOfN(num, animalGen).sample).flatten.head.toVector
 
 }
