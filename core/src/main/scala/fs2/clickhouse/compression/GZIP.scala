720af1cdb25896e6270ad78df8a7fee253629e7e
ref <yau.ref@gmail.com>
2026-08-11 21:12:49 +0300

    impl gzip compression for inserts

--- core/src/main/scala/fs2/clickhouse/compression/GZIP.scala
+++ core/src/main/scala/fs2/clickhouse/compression/GZIP.scala
@@ -11,11 +11,21 @@ object GZIP extends Compression {
 
   override val acceptEncoding: Option[String] = Some("gzip")
 
+  // gunzip() throws on empty input (it expects at least a gzip header), but
+  // an empty body is a valid, expected response (e.g. a successful insert),
+  // so an empty stream has to be short-circuited before reaching it.
   override def decompress[F[_]: Async]: Pipe[F, Byte, Byte] =
-    fs2ioCompressionForAsync[F]
-      .gunzip()
-      .andThen(_.flatMap(_.content))
+    _.pull.peek1.flatMap {
+      case None => fs2.Pull.done
+      case Some((_, stream)) =>
+        stream
+          .through(fs2ioCompressionForAsync[F].gunzip())
+          .flatMap(_.content)
+          .pull
+          .echo
+    }.stream
 
-  override def compress[F[_]: Async]: Pipe[F, Byte, Byte] = ??? // TODO: impl
+  override def compress[F[_]: Async]: Pipe[F, Byte, Byte] =
+    fs2ioCompressionForAsync[F].gzip()
 
 }
